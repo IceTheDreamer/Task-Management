79978d39b17bd7869cdb2afeb892f19a369af600
IceTheDreamer <161539958+IceTheDreamer@users.noreply.github.com>
2024-03-20 11:49:58 +0800

    Update LoginPage.js

--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -1,37 +1,42 @@
 import React, { useState } from 'react';
 import { KeyFill, PersonCircle, PersonFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
-import { db } from '../firebase';
-import {getDocs, collection} from 'firebase/firestore/lite'
 
-const usersCol = collection(db, 'users');
-const usersSnapshot = await getDocs(usersCol);
-const usersLists = usersSnapshot.docs.map(doc => doc.data().UserName);
 
 function LoginPage() {
-  const [userName, setUserName] = useState()
-  const [passWord, setPassword] = useState()
+  const [userName, setUserName] = useState('')
+  const [passWord, setPassword] = useState('')
   const [routeTo, setRouteTo] = useState()
 
-  const notify = () => {
-    if(userName === 'admin'){
-      if(passWord === 'IceTheDreamer'){
-        return(setRouteTo('/admin'))
+  const handleSubmit = async (event) => {
+    console.log("Form submitted"); // Check if form submission is working
+
+    try {
+      const response = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: userName,
+          password: passWord,
+          // expiresInMins: 60, // optional
+        })
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        if(userName === 'jissetts'){
+          return (setRouteTo('/admin'));// Handle successful login response
+        }
+        else{
+          return (setRouteTo('/employee'));
+        }
+      } else {
+        console.error('Login failed');
       }
-      else{
-        alert('Wrong password!')
-      }
+    } catch (error) {
+      console.error('Error:', error);
     }
-    else{
-      const userExists = usersLists.find(user => user === userName);
-      if(userExists){
-        return(setRouteTo('/employee'))
-      }
-      else{
-        alert('Wrong!')
-      }
-    }
-  }
+  };
   return (
     <div className='LoginPage'>
         <div className='Form'>
@@ -55,7 +60,7 @@ function LoginPage() {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-                <Link to={routeTo} type='submit' onClick={notify}>Submit</Link>
+                <Link to={routeTo} type='submit' onClick={handleSubmit}>submit</Link>
             </form>
         </div>
     </div>
